b02560f7e9de8007dcdb25bfdcfeac8a57fb1050
Gautier <gautier.fournet@gmail.com>
2024-02-13 13:21:39 +0100

    Add all the tags

--- client/src/Tagbar/Tagbar.js
+++ client/src/Tagbar/Tagbar.js
@@ -1,12 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './tagbar.module.css';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 function Tagbar(props) {
+    const [usingTag, setUsingTag] = useState("All"); 
+
+    const changeBackgroundColor = (tagName) => {
+        setUsingTag(tagName); 
+    };
 
     return (
         <>
-            <Link className={styles.tag}>{props.name}</Link>
+            {props.name.map((tag, index) => (
+                <Link
+                    key={index}
+                    to={`/${tag}`}
+                    className={usingTag === tag ? styles.tag : styles.notclickedtag}
+                    onClick={() => changeBackgroundColor(tag)}
+                >
+                    {tag}
+                </Link>
+            ))}
+
         </>
     );
 }
