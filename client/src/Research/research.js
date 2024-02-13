537823aafdc1c46ce21da614a69c1fa43aa91251
Gautier <gautier.fournet@gmail.com>
2024-02-13 16:49:02 +0100

    Modif Research

--- client/src/Research/research.js
+++ client/src/Research/research.js
@@ -1,13 +1,28 @@
 import styles from './research.module.css'
+import Tagbar from '../Tagbar/Tagbar';
+import Article from '../Article/article';
 
+function Research(props) {
 
-    function Research(props) { 
-
-        return (
+    return (
         <>
-            <h1>Research your article</h1>
+            <div className={styles.inputContainer}>
+                <input className={styles.searchInput} type="text" placeholder='SEARCH' />
+            </div>
+            <div className={styles.Alltags}>
+                <div className={styles.tagcontainer}>
+                    <Tagbar page={"research"} />
+                </div>
+            </div>
+
+            <div className={styles.padding}>
+                <h1 className={styles.title}>All</h1>
+            </div> 
+
+
+            <Article />
         </>
-        )
-    }
+    )
+}
 
 export default Research;
