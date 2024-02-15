7f022ebd356ddb45e20137215486c0b6b4bee456
Gautier <gautier.fournet@gmail.com>
2024-02-15 16:48:42 +0100

    Ajout Link

--- client/src/splash/splash.js
+++ client/src/splash/splash.js
@@ -1,30 +1,34 @@
 import styles from './splash.module.css'
 import logo from "../img/splash.png"
 import arrowIcon from "../img/arrow.svg";
+import { Link } from 'react-router-dom';
 
 function Splash() {
     return (
         <div className="Register">
             <img className={styles.img} src={logo} alt="" />
-            <h1 className={styles.title}>LOGIN OR</h1>
-            <h1 className={styles.title}>REGISTER</h1>
+            <h1 className={styles.title}>LOGIN OR REGISTER</h1>
             <form>
-                <button className={styles.button}>
+                <Link to="/login" className={styles.button}>
                     <div className={styles.arrowContainer}>
-                        <div >
+                        <div>
                             <span className={styles.buttonText}>LOGIN</span>
                         </div>
-                        <img src={arrowIcon} alt="arrow-Icon" />
+                        <div>
+                            <img src={arrowIcon} alt="arrow-Icon" />
+                        </div>
                     </div>
-                </button>
-                <button className={styles.button}>
+                </Link>
+                <Link to="/signup" className={styles.button}>
                     <div className={styles.arrowContainer}>
                         <div >
                             <span className={styles.buttonText}>REGISTER</span>
                         </div>
                         <img src={arrowIcon} alt="arrow-Icon" />
                     </div>
-                </button>
+                </Link>
+
+                <div className={styles.bottomLine}></div>
             </form>
         </div>
     )
