7b393df93b9b4022bf8ab2872d47d0a3c5fd444a
Gautier <gautier.fournet@gmail.com>
2024-02-15 10:26:14 +0100

    Ajout Import

--- client/src/App.js
+++ client/src/App.js
@@ -6,11 +6,16 @@ import Signup from './Signup/signup';
 import Login from './Login/login';
 import NoMatch from './NoMatch/nomatch';
 import Detailarticle from './Detailarticle/detailarticle';
+import Splash from './splash/splash';
+import Changename from './changename/changename';
+import Profile from './Profile/profile';
 import {Routes, Route, Link} from 'react-router-dom';
+import addarticle from './img/addArticle.svg';
 import homeimg from './img/home.svg';
 import searchimg from './img/search.svg';
 import loginimg from './img/login.svg';
 import logoutimg from './img/logout.svg';
+
 
 function App() {
   return (
@@ -24,7 +29,8 @@ function App() {
           <Link to="/research" className='link'>Search</Link>
         </div>
         <div className="icon-container">
-          <Link to="/saved" className='link'>Saved</Link>
+          <Link to="/addarticle" className='link'><img src={addarticle} className="icon" alt="Home" /></Link>
+          <span className="icon-label">Add</span>
         </div>
         <div className="icon-container">
           <Link to="/profile" className='link'>Profile</Link>
@@ -37,10 +43,14 @@ function App() {
         <Route exact={true} path="/research" element={<Research />} />
         <Route exact={true} path="/research/:id" element={<Research />} />
         <Route exact={true} path="/article" element={<Article />} />
+        <Route exact={true} path="/Addarticle" element={<Detailarticle />} />
         <Route exact={true} path="/login" element={<Login />} />
         <Route exact={true} path="/signup" element={<Signup />} />
+        <Route exact={true} path="/splash" element={<Splash />} />
+        <Route exact={true} path="/changename" element={<Changename />} />
         <Route exact={true} path="/Detailarticle" element={<Detailarticle />} />
         <Route path="*" element={<NoMatch />} />
+        <Route exact={true} path="/profile" element={<Profile />} />
       </Routes>
     </>
   );
